928e4edcfd13ae501db06857ba15c39eef121917
OfflineBot <FlintStoneLG@gmail.com>
2024-03-04 22:08:50 +0100

    x

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,19 @@ const Gamer = {
     Red: 2,
 };
 
-let active_player = Game.Blue;
+let active_player = Gamer.Blue;
+let switch_player = document.getElementById('switch-player');
+
+switch_player.style = "border-color: var(--blue-player);";
+
+switch_player.addEventListener('click', () => {
+        active_player = active_player == Gamer.Blue ? Gamer.Red : Gamer.Blue; 
+        if (active_player == Gamer.Blue) {
+            switch_player.style = "border-color: var(--blue-player);";
+        } else {
+            switch_player.style = "border-color: var(--red-player);";
+        }
+    })
 
 let field = window.game_field;
 
