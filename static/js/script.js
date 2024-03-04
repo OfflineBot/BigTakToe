5e286b8bca961a48454ea65006b8a8fb781a27b2
OfflineBot <FlintStoneLG@gmail.com>
2024-03-04 23:04:00 +0100

    select

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,19 @@ const Gamer = {
 
 let field = window.game_field;
 let last_box = "";
+let last_frame = "";
+
+function deselect() {
+    document.getElementById("field1").style = "border-color: var(--frame-color);";
+    document.getElementById("field2").style = "border-color: var(--frame-color);";
+    document.getElementById("field3").style = "border-color: var(--frame-color);";
+    document.getElementById("field4").style = "border-color: var(--frame-color);";
+    document.getElementById("field5").style = "border-color: var(--frame-color);";
+    document.getElementById("field6").style = "border-color: var(--frame-color);";
+    document.getElementById("field7").style = "border-color: var(--frame-color);";
+    document.getElementById("field8").style = "border-color: var(--frame-color);";
+    document.getElementById("field9").style = "border-color: var(--frame-color);";
+}
 
 // top color btn and active player
 let active_player = Gamer.Blue;
@@ -31,12 +44,20 @@ switch_player.addEventListener('click', () => {
 
 // apply
 document.getElementById('apply-btn').addEventListener("click", () => {
+    deselect();
     if (last_box != "") {
         if (active_player == Gamer.Blue) {
             document.getElementById(last_box).style = "background-color: var(--blue-player);";
+            game_field[last_box] = 1;
         } else {
             document.getElementById(last_box).style = "background-color: var(--red-player);";
+            game_field[last_box] = 2;
         }
+
+        let frame = "field" + last_box.slice(-1);
+        console.log(frame)
+        document.getElementById(frame).style = "border-color: var(--select-frame);";
+        
     }
     last_box = "";
     change_player();
